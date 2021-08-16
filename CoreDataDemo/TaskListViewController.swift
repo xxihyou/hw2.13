c42c3a59db1cac7774b3765feff067c28f404684
Eugeniya <bruyko.e@gmai.com>
2021-08-16 20:37:45 +0300

    1. Prepare App 2. Setup Navigation Bar

---
+++ CoreDataDemo/TaskListViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by brubru on 16.08.2021.
+//
+
+import UIKit
+
+class TaskListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        setupNavigationBar()
+    }
+
+    private func setupNavigationBar() {
+        title = "Task List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let navBarAppearence = UINavigationBarAppearance()
+        
+        navBarAppearence.configureWithOpaqueBackground()
+        navBarAppearence.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearence.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navBarAppearence.backgroundColor = UIColor(
+            red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 194/255
+        )
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearence
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearence
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addNewTask)
+        )
+        
+        navigationController?.navigationBar.tintColor = .white
+    }
+    
+    @objc private func addNewTask() {
+        
+    }
+}
+
